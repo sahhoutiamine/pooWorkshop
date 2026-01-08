e208e14b56257d3ad03e69be6e169600b012be1c
sahhoutiamine <midoriaduko@gmail.com>
2026-01-08 11:42:23 +0100

    add getAllBooks methods

--- Repositories/BookRepository.php
+++ Repositories/BookRepository.php
@@ -1,1 +1,22 @@
 <?php
+require_once "../Models/Book.php";
+require_once "../Models/Author.php";
+require_once "../Interfaces/BookRepositoryInterface.php";
+require_once "../Core/Database.php";
+
+
+class BookRepository implements BookRepositoryInterface {
+    private PDO $db;
+    
+    public function __construct() {
+        $this->db = Database::getInstance()->getConnection();
+    }
+
+    public function getAllBooks () {
+        $books = $this->db->query("SELECT * FROM books ;");
+        return $books->fetchAll();
+    }
+
+   
+
+}
